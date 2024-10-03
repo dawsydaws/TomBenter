a84f92e78f44bd38e27e76d37ebc69191e11ac12
Dilson's Pickles <a.dawson@mu.se>
2024-10-03 14:19:25 +1000

    Create console branding

--- tom-benter/app/components/Branding.tsx
+++ tom-benter/app/components/Branding.tsx
@@ -2,12 +2,37 @@ import React from "react";
 
 const styles = {
     container: {
-        backgroundColor: "darkgrey",
+        display: "flex",
     } as React.CSSProperties,
     brandFrame: {
+        display: "flex", 
+        flexDirection: "column",
+        alignItems: "end",
+        flexShrink: "1",
     } as React.CSSProperties,
-    brand: {
+    brandName: {
+        fontFamily: "serif",
+        fontWeight: "black",
         fontSize: "3rem",
+    } as React.CSSProperties,
+    brandSeries: {
+        fontStyle: "italic",
+    } as React.CSSProperties,
+    modelFrame: {
+        display: "flex", 
+        flexGrow: "1",
+        flexDirection: "column",
+        alignItems: "end",
+    } as React.CSSProperties,
+    modelName: {
+        fontWeight: "black",
+        fontSize: "3rem",
+        letterSpacing: "0.25rem",
+        textDecoration: "underline",
+
+    } as React.CSSProperties,
+    modelTagline: {
+        letterSpacing: "0.35rem"
     } as React.CSSProperties,
 };
 
@@ -15,8 +40,12 @@ function Branding() {
     return (
         <div style={styles.container}>
             <div style={styles.brandFrame}>
-                <h1 style={styles.brand}>AKAI</h1>
-                <p>professional</p>
+                <h1 style={styles.brandName}>AKAI</h1>
+                <p style={styles.brandSeries}>professional</p>
+            </div>
+             <div style={styles.modelFrame}>
+                <h1 style={styles.modelName}>MPC2000XL</h1>
+                <p style={styles.modelTagline}>MIDI PRODUCTION CENTER</p>
             </div>
         </div>
     );
